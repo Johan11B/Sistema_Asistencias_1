22b49c48a1beb7904cf87b4df2da4eb8b84b25a0
Johan11B <jbriseno@ucundinamarca.edu.co>
2025-05-25 13:38:26 -0500

    Cambios

--- Backend/routes/estudiantesroutes.js
+++ Backend/routes/estudiantesroutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const estudiantesController = require("../controllers/estudiantescontrollers");
 
-// Consultar estudiante (GET)
+// Rutas para estudiantes
 router.get("/", estudiantesController.consultar);
-
-// Registrar nuevo estudiante (POST)
 router.post("/", estudiantesController.ingresar);
-
-// Modificar estudiante (PUT)
 router.put("/", estudiantesController.modificar);
+
+// Rutas para asignaturas
+router.get("/asignatura", estudiantesController.consultarAsignatura);
+router.post("/asignatura", estudiantesController.agregarEstudianteAsignatura);
 
 module.exports = router;
