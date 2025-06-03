2086314843ba6b02ab3bb504b767517d5ac7f337
Johan11B <jbriseno@ucundinamarca.edu.co>
2025-06-02 20:38:27 -0500

    Nuevos Cambios

--- Netlify/functions/departamento.js
+++ Netlify/functions/departamento.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var cors = require("cors");
-var serverless = require ('serverless-http');
-var port = process.env.PORT || 5000;
+var serverless = require('serverless-http');
 var app = express();
 var departamentoroutes = require("../../Backend/routes/departamentoroutes.js");
+
 app.use(express.json());
 app.use(cors());
 
+// Usa el router exactamente como en tu ejemplo funcional
 var router = express.Router();
-router.use ("/departamento",departamentoroutes);
-var handler = app.use ('/.netlify/functions',router);
-exports.handler = serverless (app);
+router.use("/departamento", departamentoroutes); // <- Cambiado a "departamento"
+
+// Esta línea debe ser exactamente así como en tu ejemplo que funciona
+app.use('/.netlify/functions', router);
+
+// Exportación idéntica a tu ejemplo funcional
+exports.handler = serverless(app);
